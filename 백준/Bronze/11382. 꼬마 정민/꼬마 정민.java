d574395a47ed50b1c8e98036273985d712668fcf
qlek1108 <89445477+qlek1108@users.noreply.github.com>
2023-08-18 16:18:26 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner s=new Scanner(System.in);
+		
+		long a = s.nextLong();
+		long c = s.nextLong();
+		long d = s.nextLong();
+		
+		System.out.println(a+d+c);
+ 
+	}
+}
